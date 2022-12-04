5163d083c257f44f2536b3c2b2c7ac50b8228615
Hounaar <95884627+hounaar@users.noreply.github.com>
2022-12-04 15:09:05 +0330

    Add files via upload

---
+++ main/orders/addfield.js
@@ -0,0 +1,32 @@
+var firstadd = document.getElementById('first');
+var secadd = document.getElementById('second');
+var third = document.getElementById('third');
+var first_btnadd = document.getElementById('first_btnadd');
+
+function show_firstadd() {
+    firstadd.style.display = "block";
+    first_btnadd.style.display = "none";
+}
+
+function show_secadd() {
+    secadd.style.display = "block";
+}
+
+function show_thirdadd() {
+    third.style.display = "block";
+
+}
+
+function remove_firstadd() {
+    firstadd.style.display = "none";
+
+}
+
+function remove_secadd() {
+    secadd.style.display = "none";
+}
+
+function remove_thirdadd() {
+    secadd.style.display = "none";
+
+}
